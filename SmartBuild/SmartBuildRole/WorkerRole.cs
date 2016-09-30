b567e6473a6bec389df2818f5b7b839f0f5a98ab
Andrey Mitsyk <mitzyk@mail.ru>
2016-09-30 16:08:53 +0300

    Fix

--- SmartBuild/SmartBuildRole/WorkerRole.cs
+++ SmartBuild/SmartBuildRole/WorkerRole.cs
@@ -13,7 +13,7 @@ namespace SmartBuildRole
     {
         private readonly CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         private readonly ManualResetEvent runCompleteEvent = new ManualResetEvent(false);
-        private const string hubNames = "smartbuildhub";
+        private const string hubName = "smartbuildhub";
         NotificationHubClient hub = null;
         private string buildId = "0";
 
